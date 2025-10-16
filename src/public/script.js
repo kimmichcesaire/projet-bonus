6f178c70693882d677ed10c93449f7c082e6eabc
kimmichcesaire <131315628+kimmichcesaire@users.noreply.github.com>
2025-10-16 21:40:04 +0100

    new modif2

--- src/public/script.js
+++
@@ -1,8 +0,0 @@
-async function chargerCitation() {
-    const response = await fetch('https://zenquotes.io/api/random');
-    const data = await response.json();
-    document.getElementById('citation').innerText = `${data[0].q} - ${data[0].a}`;
-}
-
-document.getElementById('nouvelle').addEventListener('click', chargerCitation);
-window.onload = chargerCitation;
